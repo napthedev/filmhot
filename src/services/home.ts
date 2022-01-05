107d5c4d7e0122223d428dfbd378e3ce36344e48
napthedev <phongna.dev@gmail.com>
2022-01-05 20:24:54 +0700

    Feature: retry home on the backup worker

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,15 +1,28 @@
 import { HomeSection, TopSearched } from "../shared/types";
 
 import axios from "../shared/axios";
+import normalAxios from "axios";
 
-export const getHome = async (page: number = 0): Promise<HomeSection[]> =>
-  (
-    await axios.get("homePage/getHome", {
-      params: {
-        page,
-      },
-    })
-  ).data.data.recommendItems;
+export const getHome = async (page: number = 0): Promise<HomeSection[]> => {
+  try {
+    const data = (
+      await axios.get("homePage/getHome", {
+        params: {
+          page,
+        },
+      })
+    ).data.data.recommendItems;
+
+    if (data) return data;
+    else throw new Error("");
+  } catch (error) {
+    const data = (
+      await normalAxios.get("https://filmhot-backup.napdev.workers.dev")
+    ).data;
+
+    return data;
+  }
+};
 
 export const getTopSearched = async (): Promise<TopSearched[]> =>
   (await axios.get("search/v1/searchLeaderboard")).data.data.list;
