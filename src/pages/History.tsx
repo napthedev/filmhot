3ea121326ee3fa187fd648ffcd7473fdf6209205
napthedev <phongna.dev@gmail.com>
2022-01-17 07:21:39 +0700

    Feature: Add info when no watch history found

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -35,41 +35,47 @@ const History: FC = () => {
             <i className="fas fa-trash"></i> <span>Clear</span>
           </button>
         </div>
-        <div className="grid gap-6 grid-cols-sm md:grid-cols-lg">
-          {data.length === 0 ? (
-            <div>No result found</div>
-          ) : (
-            <>
-              {data.map((item) => (
-                <Link
-                  title={item.name}
-                  to={
-                    item.category === 0 ? `/movie/${item.id}` : `/tv/${item.id}`
-                  }
-                  key={item.id}
-                  className="relative h-0 pb-[163%] bg-dark-lighten rounded overflow-hidden group"
-                >
-                  <div className="absolute top-0 left-0 w-full h-full flex flex-col items-stretch">
-                    <div className="relative w-full h-0 pb-[140%] flex-shrink-0 group-hover:brightness-[80%] transition duration-300">
-                      <LazyLoadImage
-                        effect="opacity"
-                        className="absolute top-0 left-0 w-full h-full object-cover"
-                        src={resizeImage(item.coverVerticalUrl, "250")}
-                        alt=""
-                      />
-                    </div>
+        {data.length === 0 ? (
+          <div className="flex flex-col items-center my-10 gap-6">
+            <img className="w-40 h-40 object-cover" src="/cinema.svg" alt="" />
 
-                    <div className="flex-grow flex items-center">
-                      <h1 className="w-full whitespace-nowrap overflow-hidden text-ellipsis px-2 group-hover:text-primary transition duration-300">
-                        {item.name}
-                      </h1>
-                    </div>
+            <p className="text-xl">No Watch history found</p>
+
+            <Link className="text-xl text-primary" to="/">
+              Discover more
+            </Link>
+          </div>
+        ) : (
+          <div className="grid gap-6 grid-cols-sm md:grid-cols-lg">
+            {data.map((item) => (
+              <Link
+                title={item.name}
+                to={
+                  item.category === 0 ? `/movie/${item.id}` : `/tv/${item.id}`
+                }
+                key={item.id}
+                className="relative h-0 pb-[163%] bg-dark-lighten rounded overflow-hidden group"
+              >
+                <div className="absolute top-0 left-0 w-full h-full flex flex-col items-stretch">
+                  <div className="relative w-full h-0 pb-[140%] flex-shrink-0 group-hover:brightness-[80%] transition duration-300">
+                    <LazyLoadImage
+                      effect="opacity"
+                      className="absolute top-0 left-0 w-full h-full object-cover"
+                      src={resizeImage(item.coverVerticalUrl, "250")}
+                      alt=""
+                    />
                   </div>
-                </Link>
-              ))}
-            </>
-          )}
-        </div>
+
+                  <div className="flex-grow flex items-center">
+                    <h1 className="w-full whitespace-nowrap overflow-hidden text-ellipsis px-2 group-hover:text-primary transition duration-300">
+                      {item.name}
+                    </h1>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
