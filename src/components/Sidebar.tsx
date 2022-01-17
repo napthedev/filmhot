f64c38d02f4baaea18b4805118e2dbc5175199a5
napthedev <phongna.dev@gmail.com>
2022-01-17 19:42:55 +0700

    Style: Fix responsive sidebar position

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,8 +23,8 @@ const Sidebar: FC<SidebarProps> = ({ sidebarActive, setSidebarActive }) => {
   return (
     <>
       <div
-        className={`flex-shrink-0 sm:sticky sm:!left-0 fixed top-0 flex flex-col items-stretch py-10 pl-5 xl:pl-10 pr-0 w-[90vw] max-w-[288px] sm:max-w-none sm:w-16 xl:w-72 border-r border-gray-800 h-screen overflow-y-auto z-10 bg-dark sm:bg-transparent transition-all duration-500 ${
-          sidebarActive ? "left-0" : "left-[-100%]"
+        className={`flex-shrink-0 sm:sticky left-auto right-full sm:!right-0 sm:!left-0 fixed top-0 flex flex-col items-stretch py-10 pl-5 xl:pl-10 pr-0 w-[90vw] max-w-[288px] sm:max-w-none sm:w-16 xl:w-72 border-r border-gray-800 h-screen overflow-y-auto z-10 bg-dark sm:bg-transparent sm:!translate-x-0 transition-all duration-500 ${
+          sidebarActive ? "translate-x-full" : "translate-x-0"
         }`}
       >
         <Link to="/" className="flex gap-2 items-center">
@@ -139,7 +139,7 @@ const Sidebar: FC<SidebarProps> = ({ sidebarActive, setSidebarActive }) => {
       </div>
       <div
         onClick={() => setSidebarActive(false)}
-        className={`bg-[#00000080] z-[5] fixed top-0 left-0 w-full h-full transition duration-500 md:!opacity-0 ${
+        className={`bg-[#00000080] z-[5] fixed top-0 left-0 w-full h-full transition-all duration-500 sm:!opacity-0 ${
           sidebarActive ? "opacity-100 visible" : "opacity-0 invisible"
         }`}
       ></div>
