b57b034284b65e92a61890f0cb7d458694fc61d3
napthedev <phongna.dev@gmail.com>
2022-08-21 16:04:22 +0700

    Chore: remove next image from next config

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ const { withSuperjson } = require("next-superjson");
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
-  },
-  images: {
-    domains: ["img.netpop.app"],
-    minimumCacheTTL: 99999,
-  },
 };
 
 module.exports = nextConfig;
